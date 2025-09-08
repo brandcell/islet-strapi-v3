06320a22890babc3c769c0a50af79cd49d073622
brianooi <briansh1225@gmail.com>
2025-09-09 01:19:18 +0800

    migrated to v5

--- config/plugins.js
+++ config/plugins.js
@@ -15,29 +15,7 @@ module.exports = ({ env }) => ({
 
   // ...
   upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
-    },
+    config: {},
   },
-  "mux-video-uploader": {
-    enabled: true,
-    config: {
-      accessTokenId: env('ACCESS_TOKEN_ID'),
-      secretKey: env('ACCESS_TOKEN_SECRET'),
-      webhookSigningSecret: env('WEBHOOK_SIGNING_SECRET'),
-      // playbackSigningId: env('SIGNING_KEY_ID'),
-      // playbackSigningSecret: env('SIGNING_KEY_PRIVATE_KEY'),
-    }
-  }
   // ...
 });
